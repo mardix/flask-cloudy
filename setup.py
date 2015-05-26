584a2acdb889205cf91e6ce9365a7c5bbf8932e0
Mardix <mcx2082@gmail.com>
2015-05-26 02:57:17 -0400

    0.3.0 - Fix version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages
 
 
 __NAME__ = "flask-CloudStorage"
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 __author__ = "Mardix"
 __license__ = "MIT"
 __copyright__ = "2015"
