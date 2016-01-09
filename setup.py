d60939920e7c3702c142c45da50eabd73b93c8c5
Mardix <mardix@pylot.io>
2016-01-09 05:44:39 -0500

    0.13.1 - Merge pull request

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ Supported storage:
 from setuptools import setup, find_packages
 
 __NAME__ = "Flask-Cloudy"
-__version__ = "0.13.0"
+__version__ = "0.13.1"
 __author__ = "Mardix"
 __license__ = "MIT"
 __copyright__ = "2015"
